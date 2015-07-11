cb5053a6337bb0d0a83ce2376b1f842f11a81163
lexnewgate <alex@alexs-mbp-3.gateway.2wire.net>
2015-07-11 15:13:34 +0100

    update
    新建了 可控的camera
    更改了文件组织

---
+++ code/Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+	public Camera autoFollowCam;
+	public Camera roamingCam;
+	public Camera controllableFollowCam;
+	// Use this for initialization
+	void Start () {
+		autoFollowCam.enabled = true;
+		roamingCam.enabled = false;
+		controllableFollowCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.V) && autoFollowCam.enabled) {
+			autoFollowCam.enabled=false;
+			roamingCam.enabled=true;
+		}
+		else if(Input.GetKeyDown (KeyCode.V) && roamingCam.enabled)
+		{
+			autoFollowCam.enabled=true;
+			roamingCam.enabled=false;
+		}
+	}
+}
