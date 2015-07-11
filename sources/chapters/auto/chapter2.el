d90331e9dfda5e08a81cb4bcf04aa3b5b5337f33
lexnewgate <alex@alexs-mbp-3.gateway.2wire.net>
2015-07-11 04:18:08 +0100

    thesis

---
+++ sources/chapters/auto/chapter2.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "chapter2"
+ (lambda ()
+   (LaTeX-add-labels
+    "fig:graph1"
+    "fig:graph3"
+    "fig:scenegraph2"
+    "fig:rigging"
+    "fig:movingRobot-a"
+    "fig:movingRobot-b"
+    "fig:movingRobot-c"
+    "fig:movingRobot-d"
+    "fig:movingRobot"
+    "fig:fk_ik"
+    "fig:twolink"
+    "fig:fur"
+    "fig:fins")))
+
