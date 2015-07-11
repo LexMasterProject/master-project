2c308c74be841c55b1c582f0d27f9591da3d35d6
lexnewgate <alex@alexs-mbp-3.gateway.2wire.net>
2015-07-11 17:29:12 +0100

    可控相机跟随完成

---
+++ code/Assets/Scripts/Camera/AutoFollowCamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoFollowCamera : MonoBehaviour {
+
+	public float distanceAway;
+	public float distanceUp;
+	public Transform follow;
+	public float smooth;
+
+	private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+
+		//init camera variables
+		distanceAway = 10.0f;
+		distanceUp = 30.0f;
+		smooth = 0.1f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void LateUpdate()
+	{
+
+			targetPosition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+
+			Debug.DrawRay (follow.position, Vector3.up * distanceUp, Color.red);
+			Debug.DrawRay (follow.position, -1f * follow.forward * distanceAway, Color.blue);
+			Debug.DrawLine (follow.position, targetPosition, Color.green);
+
+			transform.position = Vector3.Lerp (transform.position, targetPosition, Time.deltaTime * smooth);
+			transform.LookAt (follow);
+
+	}
+}
---
+++ code/Assets/Scripts/Camera/ControllableFollowCam.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllableFollowCam : MonoBehaviour {
+	public float distanceAway;
+	public float distanceUp;
+	public Transform follow;
+	// Use this for initialization
+	void Start () {
+		//init camera variables
+		distanceAway = 10.0f;
+		distanceUp = 30.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void LateUpdate()
+	{
+		transform.position = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+		transform.LookAt (follow);
+	}
+}
