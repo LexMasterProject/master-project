8fa4b3d9407b21f32a8feb595d15a5fc0e45e3df
lexnewgate <alex@alexs-mbp-3.gateway.2wire.net>
2015-07-11 04:13:32 +0100

    initial follow camera finish

---
+++ code/Assets/mySpider/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+	public Camera followCam;
+	public Camera roamingCam;
+	// Use this for initialization
+	void Start () {
+		followCam.enabled = true;
+		roamingCam.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.V) && followCam.enabled) {
+			followCam.enabled=false;
+			roamingCam.enabled=true;
+		}
+		else if(Input.GetKeyDown (KeyCode.V) && roamingCam.enabled)
+		{
+			followCam.enabled=true;
+			roamingCam.enabled=false;
+		}
+	}
+}
