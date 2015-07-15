6e2e362d8fb52319ce4ffbc377ebdd4c3cdf8c65
lexnewgate <alex@dyn239181.shef.ac.uk>
2015-07-15 19:14:22 +0100

    相机bug修复完成
    参照点坐标选定问题

--- code/Assets/Scripts/Camera/ControllableFollowCam.cs
+++ code/Assets/Scripts/Camera/ControllableFollowCam.cs
@@ -28,7 +28,7 @@ public class ControllableFollowCam : MonoBehaviour {
 			float leftRight= -Input.GetAxis("RotateLR")*rotateSpeed*Time.deltaTime;
 			float upDown=Input.GetAxis("RotateUD")*rotateSpeed*Time.deltaTime;
 			transform.RotateAround (follow.position, follow.up, leftRight);
-			transform.RotateAround (follow.position, follow.right, upDown);
+			transform.RotateAround (follow.position, transform.right, upDown);
 
 
 		}
