c26135395cdd8593112c501b0f32b96af533ff08
lexnewgate <lexnewgate@gmail.com>
2015-07-29 02:59:26 +0100

    update

---
+++ code副本 3/Assets/Scripts/Camera/CameraSwitch.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSwitch : MonoBehaviour {
+
+	public Camera autoFollowCam;
+	public Camera roamingCam;
+	public Camera controllableFollowCam;
+	public static int activeIndex;
+	// Use this for initialization
+	void Start () {
+		autoFollowCam.enabled = true;
+		roamingCam.enabled = false;
+		controllableFollowCam.enabled = false;
+		activeIndex = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("ChangeCam") && autoFollowCam.enabled) {
+			activeIndex=1;
+			controllableFollowCam.enabled = true;
+			autoFollowCam.enabled = false;
+			roamingCam.enabled = false;
+		} else if (Input.GetButtonDown("ChangeCam") && controllableFollowCam.enabled) {
+			activeIndex=2;
+			roamingCam.enabled = true;
+			controllableFollowCam.enabled = false;
+			autoFollowCam.enabled = false;
+		} else if (Input.GetButtonDown("ChangeCam") && roamingCam.enabled) {
+			activeIndex=0;
+			autoFollowCam.enabled=true;
+			roamingCam.enabled=false;
+			controllableFollowCam.enabled=false;
+		}
+	}
+}
---
+++ code副本 3/Assets/Scripts/Camera/ControllableFollowCam.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllableFollowCam : MonoBehaviour {
+
+	public Transform follow;
+	public float dist;//distance between spider and camera
+	public Vector3 directionSTC;//spider to camera
+	public Vector3 targetPos;
+	public float zoomSpeed=10;
+	public float rotateSpeed;
+
+	private Camera camSelf;
+	// Use this for initialization
+	void Start () {
+		directionSTC = new Vector3 (0, 1, -1);
+		directionSTC.Normalize ();
+		dist = 50;
+		zoomSpeed = 10;
+		rotateSpeed = 30;
+
+		camSelf = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (camSelf.enabled) {
+			float distDiff = Input.GetAxis ("Zoom") * zoomSpeed * Time.deltaTime;
+			dist-=distDiff;
+			float leftRight= -Input.GetAxis("RotateLR")*rotateSpeed*Time.deltaTime;
+			float upDown=-Input.GetAxis("RotateUD")*rotateSpeed*Time.deltaTime;
+			transform.RotateAround (follow.position, follow.up, leftRight);
+			transform.RotateAround (follow.position, transform.right, upDown);
+		}
+	}
+
+	void LateUpdate()
+	{
+		if (camSelf.enabled) {
+			directionSTC = transform.position - follow.transform.position;
+			directionSTC.Normalize ();
+			targetPos = follow.position + directionSTC * dist;
+
+			transform.position = targetPos;
+			transform.LookAt (follow);
+			//Debug.DrawLine (follow.position, targetPos, Color.green);
+		}
+	}
+}
