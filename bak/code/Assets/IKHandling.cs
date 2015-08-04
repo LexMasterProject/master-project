76af8b8f7c5f94b043926ad940c0a256a5d79576
lexnewgate <lexnewgate@gmail.com>
2015-08-04 21:22:28 +0100

    update

---
+++ bak/code/Assets/IKHandling.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IKHandling : MonoBehaviour {
+
+	public Transform t1;
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("here");
+	}
+
+	void LateUpdate()
+	{
+		Debug.Log("late");
+		Debug.Log (t1.position);
+		if (t1.position.y < 1) {
+			t1.position = new Vector3 (t1.position.x, 1, t1.position.z);
+		}
+	}
+
+
+}
